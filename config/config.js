35996c7bee08738e272312f360a277a9d173a6a2
jhLim97 <wnsgur2749@naver.com>
2021-02-23 01:39:17 +0900

    설정 파일에 데이터베이스 관련 설정 추가

--- config/config.js
+++ config/config.js
@@ -2,7 +2,8 @@ module.exports = {
     server_port:3300,
     db_url:'mongodb://localhost:27017/shopping',
     db_schemas: [
-        {file:'./order_schema', collection:'shopping1', schemaName:'ShoppingSchema', modelName:'ShoppingModel'}
+        {file:'./order_schema', collection:'order1', schemaName:'ShoppingSchema', modelName:'ShoppingModel'},
+        {file:'./user_schema', collection:'user1', schemaName:'UserSchema', modelName:'UserModel'}
     ],
     route_info: [
         {file:'./clinic', path:'/process/listclinic_seonbyeol', method:'listclinic_seonbyeol', type:'post'},
