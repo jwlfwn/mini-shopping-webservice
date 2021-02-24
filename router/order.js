e01f8ca5675b20d8b4346be18d73337422d755b6
jhLim97 <wnsgur2749@naver.com>
2021-02-25 02:31:41 +0900

    메인화면에서 상품 클릭 시 해당 상품 정보가 담긴 결제 창으로 이동

--- router/order.js
+++ router/order.js
@@ -7,52 +7,27 @@ var selectItem = function(req, res) {
     var paramPrice = req.body.price || req.query.price;
     
     console.log('요청 검색어 : ' + paramImage + ' ' + paramName + ' ' + paramPrice);
-    /*
-    var database = req.app.get('database');
-    if(database) {           
-        database.ClinicModel.findByKeyword(paramKeyword, '99',
-        function(err, results) {
-            if(err) {
-               console.log('에러 발생.');
-               res.writeHead(200, {"Content-Type":"text/html;charset=utf8"});
-               res.write('<h1>에러 발생</h1>');
-               res.end();
-               return;
-           } 
-            
-            if(results) {
-                var context = {
-                    results:results
-                };
-                req.app.render('index', context, function(err, html) {
-                    if(err) {
-                        console.error('뷰 렌더링 중 에러 발생 : ' + err.stack);
-                        console.log('에러 발생.');
+    
+    var context = {
+        image:paramImage,
+        name:paramName,
+        price:paramPrice
+    };
+    req.app.render('pay', context, function(err, html) {
+        if(err) {
+            console.error('뷰 렌더링 중 에러 발생 : ' + err.stack);
+            console.log('에러 발생.');
 
-                        res.writeHead(200, {"Content-Type":"text/html;charset=utf8"});
-                        res.write('<h1>뷰 렌더링 중 에러 발생</h1>');
-                        res.write('<br><p>' + err.stack + '<p>');
-                        res.end();
-                        return;
-                    }
-                
-                    res.writeHead(200, {"Content-Type":"text/html;charset=utf8"});
-                    res.end(html);
-                });
-                
-            } else {
-                console.log('에러 발생.');
-                res.writeHead(200, {"Content-Type":"text/html;charset=utf8"});
-                res.write('<h1>검색 실패.</h1>');
-                res.end();
-            }
-        })
-    } else {
-        console.log('에러 발생.');
+            res.writeHead(200, {"Content-Type":"text/html;charset=utf8"});
+            res.write('<h1>뷰 렌더링 중 에러 발생</h1>');
+            res.write('<br><p>' + err.stack + '<p>');
+            res.end();
+            return;
+        }
+
         res.writeHead(200, {"Content-Type":"text/html;charset=utf8"});
-        res.write('<h1>데이터베이스 연결 안됨.</h1>');
-        res.end();
-    }*/
+        res.end(html);
+    });  
 };
 
 module.exports.selectItem = selectItem;
