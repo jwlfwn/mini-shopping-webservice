3064e316ccaff1871423cc00197632938194cc84
jhLim97 <wnsgur2749@naver.com>
2021-02-26 00:18:06 +0900

    결제 요청 시 결제 요청 전 주문 정보 먼저 저장

--- router/order.js
+++ router/order.js
@@ -1,4 +1,6 @@
 var iamport = require('../config/iamport');
+//var multipart = require('connect-multiparty');
+//var multipartMiddleware = multipart();
 
 var selectItem = function(req, res) {
     console.log('/process/selectItem 라우팅 함수 호출됨.');
@@ -33,4 +35,22 @@ var selectItem = function(req, res) {
     });  
 };
 
+var requestPay = function(req, res) {
+    console.log('/process/requestPay 라우팅 함수 호출됨.');
+    
+    // 품목 수량, 상품 이름, 수량, 고객 이메일, 이름, 번호
+    var merchant_uid = req.body.uid;
+    var product_name = req.body.product_name;
+    var amount = req.body.amount;
+    var buyer_email = req.body.buyer_email;
+    var buyer_name = req.body.buyer_name;
+    var buyer_tel = req.body.buyer_tel;
+    
+    
+    console.log('요청 검색어 : ' + merchant_uid + '-' + product_name + '-' + amount + '-' + buyer_email + '-' + buyer_name + '-' + buyer_tel);
+    
+     
+};
+
 module.exports.selectItem = selectItem;
+module.exports.requestPay = requestPay;
